60651fa7e30ea22651695e61abcbd2f0453c8398
PrabhmeharSingh <prabhmeharsinghkharbanda@gmail.com>
2023-10-10 14:29:59 +0530

    Added Color Field to Shape

--- app/src/main/java/com/example/honeybadgerslab/Shape.java
+++ app/src/main/java/com/example/honeybadgerslab/Shape.java
@@ -3,10 +3,12 @@ package com.example.honeybadgerslab;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color;
 
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
+        color="blue";
     }
 
     public int getX() {
