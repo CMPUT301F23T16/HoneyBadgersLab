9a8fe93b496108782313812f13c23729641c9314
skgandhi165 <skgandhi165@gmail.com>
2023-10-09 23:35:07 -0600

    adding Square.java

--- app/src/main/java/com/example/honeybadgerslab/Rectangle.java
+++ app/src/main/java/com/example/honeybadgerslab/Rectangle.java
@@ -1,29 +1,7 @@
-
 package com.example.honeybadgerslab;
-public class Rectangle extends Shape{
 
-    private int length;
-    private int width;
-
-    public Rectangle(int x, int y, int length, int width) {
-        super(x, y);
-        this.length = length;
-        this.width = width;
-    }
-
-    public int getLength() {return length;}
-
-    public void setLength(int length) {this.length = length;}
-
-    public int getWidth() {return width;}
-
-    public void setWidth(int width) {this.width = width;}
-
-    public double calculateArea() {
-        return length*width;
-    }
-
-    public double calculateCircumference() {
-        return 2 * (length + width);
+public class Rectangle extends Shape {
+    public Rectangle(int x, int y) {
+        super(x,y);
     }
 }
