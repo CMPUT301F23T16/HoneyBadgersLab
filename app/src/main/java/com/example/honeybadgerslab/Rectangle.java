174026dde0ba8f75b7dfbbfb131b5a4ea90b805c
skgandhi165 <67382333+skgandhi165@users.noreply.github.com>
2023-10-09 20:13:37 -0600

    Add files via upload

---
+++ app/src/main/java/com/example/honeybadgerslab/Rectangle.java
@@ -0,0 +1,29 @@
+package com.example.honeybadgerslab;
+
+public class Rectangle extends Shape{
+
+    private int length;
+    private int width;
+
+    public Rectangle(int x, int y, int length, int width) {
+        super(x, y);
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getLength() {return length;}
+
+    public void setLength(int length) {this.length = length;}
+
+    public int getWidth() {return width;}
+
+    public void setWidth(int width) {this.width = width;}
+
+    public double calculateArea() {
+        return length*width;
+    }
+
+    public double calculateCircumference() {
+        return 2 * (length + width);
+    }
+}
