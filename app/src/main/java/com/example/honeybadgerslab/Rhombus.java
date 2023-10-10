f0993a0ba6913a54e36413795d9c3f16dab41142
PrabhmeharSingh <prabhmeharsinghkharbanda@gmail.com>
2023-10-10 14:16:40 +0530

    Changed Rhombus.java

--- app/src/main/java/com/example/honeybadgerslab/Rhombus.java
+++ app/src/main/java/com/example/honeybadgerslab/Rhombus.java
@@ -1,2 +1,9 @@
-package com.example.honeybadgerslab;public class Rhombus {
+package com.example.honeybadgerslab;
+
+public class Rhombus extends Shape {
+    public Rhombus(int x)
+    {
+        super(x,x);
+    }
+
 }
